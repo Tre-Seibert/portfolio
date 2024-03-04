39988f88a5daadbdda68cae419dc35b5c853b916
Tre Seibert <63426280+Tre-Seibert@users.noreply.github.com>
2024-03-04 12:11:57 -0500

    .

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(../public/index.html);
 });
 
 app.listen(port, () => {
