70c4e4ec6e2825f0781d9d51cae3d76fcfd5f20f
Tre Seibert <63426280+Tre-Seibert@users.noreply.github.com>
2024-03-11 18:50:13 -0400

    updated for pages

--- README.md
+++
@@ -1,5 +0,0 @@
-# portfolio
-
-
-my computer science career in a website. also, my two cents on cs related 
-topics...
--- dist/public/index.js
+++ dist/public/index.js
@@ -1,3 +1,4 @@
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 // Set up the scene
 const scene = new THREE.Scene();
 
